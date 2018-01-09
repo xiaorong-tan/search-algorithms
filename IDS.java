74fab4d783a494cc5cc8fce5d34477bcf31187a7
xiaorong-tan <tanxiaorong104@hotmail.com>
2018-01-09 07:04:03 +0000

    Update IDS.java

--- IDS.java
+++ IDS.java
@@ -27,8 +27,7 @@ public class IDS {
         if (newState != null) {
             nodeStack.push(newState);
             node++;
-            newState.setDepthLevel(currentState.getDepthLevel() + 1);
-            //System.out.println("子节点层数：" + newState.getDepthLevel());
+            newState.setDepthLevel(currentState.getDepthLevel() + 1);            
         }
 
 
@@ -36,25 +35,21 @@ public class IDS {
         if (newState != null) {
             nodeStack.push(newState);
             node++;
-            newState.setDepthLevel(currentState.getDepthLevel() + 1);
-            //System.out.println("子节点层数：" + newState.getDepthLevel());
+            newState.setDepthLevel(currentState.getDepthLevel() + 1);            
         }
 
         newState = b.moveDown(currentState);
         if (newState != null) {
             nodeStack.push(newState);
             node++;
-            newState.setDepthLevel(currentState.getDepthLevel() + 1);
-            //System.out.println("子节点层数：" + newState.getDepthLevel());
+            newState.setDepthLevel(currentState.getDepthLevel() + 1);            
         }
 
         newState = b.moveLeft(currentState);
         if (newState != null) {
             nodeStack.push(newState);
             node++;
-            newState.setDepthLevel(currentState.getDepthLevel() + 1);
-            //System.out.println("子节点层数：" + newState.getDepthLevel());
-
+            newState.setDepthLevel(currentState.getDepthLevel() + 1);           
         }
 
     }
@@ -64,23 +59,18 @@ public class IDS {
         while (!findSolution) {
             try {
                 nodeStack.push(startState);
-                limit++;
-                //depth = 0;
+                limit++;                
                 startState.setDepthLevel(0);
                 while (!nodeStack.isEmpty()) {
-                    moveCounter++;
-                    //System.out.println("Step:" + moveCounter);
+                    moveCounter++;                   
                     currentState = nodeStack.pop();
                     System.out.println("Depth: " + currentState.getDepthLevel() + "----------------\n" + currentState.showState() + "----------------------------\n");
-
-                    //System.out.println("Depth：" + limit);
                     if (currentState.getX(1) == goalState.getX(1) && currentState.getY(1) == goalState.getY(1) && currentState.getX(2) == goalState.getX(2) && currentState.getY(2) == goalState.getY(2) && currentState.getX(3) == goalState.getX(3) && currentState.getY(3) == goalState.getY(3)) {
                         findSolution = true;
                         System.out.println("Reached goal state!\n" + currentState.showState());
                         break;
                     } else if (currentState.getDepthLevel() < limit) {
-                        moveAction(currentState);
-                        //depth++;
+                        moveAction(currentState);                        
                     }
                 }
             } catch (Exception e) {
@@ -98,31 +88,24 @@ public class IDS {
         boolean visited;
         Node tempNode;
         while (!findSolution) {
-            try {
-                //visited = false;
+            try {                
                 nodeStack.push(startState);
                 visit.clear();
-                limit++;
-                //depth = 0;
+                limit++;                
                 startState.setDepthLevel(0);
                 while (!nodeStack.isEmpty()) {
                     moveCounter++;
-                    visited = false;
-                    //System.out.println("Step:" + moveCounter);
+                    visited = false;                    
                     currentState = nodeStack.pop();
                     for (int i = 0; i < visit.size(); i++) {
                         tempNode = visit.get(i);
                         if (currentState.showState().equals(tempNode.showState())) {
-                            visited = true;
-                            //System.out.println("保存的与首元素相同的元素：" + tempNode.showState());
-                            //System.out.println("elements in queue:" + elements);
+                            visited = true;                           
                             break;
                         }
                     }
                     if (!visited) {
-                        System.out.println("Depth: " + currentState.getDepthLevel() + "----------------\n" + currentState.showState() + "----------------------------\n");
-
-                        //System.out.println("Depth：" + limit);
+                        System.out.println("Depth: " + currentState.getDepthLevel() + "----------------\n" + currentState.showState() + "----------------------------\n");                      
                         if (currentState.getX(1) == goalState.getX(1) && currentState.getY(1) == goalState.getY(1) && currentState.getX(2) == goalState.getX(2) && currentState.getY(2) == goalState.getY(2) && currentState.getX(3) == goalState.getX(3) && currentState.getY(3) == goalState.getY(3)) {
                             findSolution = true;
                             System.out.println("Reached goal state!\n" + currentState.showState());
@@ -130,8 +113,6 @@ public class IDS {
                         } else if (currentState.getDepthLevel() < limit) {
                             moveAction(currentState);
                             visit.add(currentState);
-
-                            //depth++;
                         }
                     }
                 }
